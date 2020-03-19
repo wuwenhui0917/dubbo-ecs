04d09074b5844b9f956bf692e64cd8d00c01088b
wuwenhui0917 <wuwenhui0917@163.com>
2020-03-19 22:12:09 +0800

    事务添加

--- dubbo-arrage/src/main/java/com/ai/ecs/dubbo/ta/imp/LocalTransaction.java
+++ dubbo-arrage/src/main/java/com/ai/ecs/dubbo/ta/imp/LocalTransaction.java
@@ -4,6 +4,7 @@ import javax.transaction.InvalidTransactionException;
 import javax.transaction.TransactionRequiredException;
 import javax.transaction.TransactionRolledbackException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Copyright asiainfo.com
@@ -15,20 +16,40 @@ public class LocalTransaction extends AbstractTransaction {
     private Connection currentConnection =null;
 
     public LocalTransaction(Connection connection){
+        this.currentConnection = connection;
 
     }
     @Override
     public void begin() throws InvalidTransactionException {
-
+        if(this.currentConnection!=null){
+            try {
+                currentConnection.setAutoCommit(false);
+            } catch (SQLException e) {
+                throw new InvalidTransactionException();
+            }
+        }
     }
 
     @Override
     public void commit() throws TransactionRequiredException {
+        if(this.currentConnection!=null){
+            try {
+                currentConnection.commit();
+            } catch (SQLException e) {
+                throw new TransactionRequiredException();
+            }
+        }
 
     }
 
     @Override
     public void rollback() throws TransactionRolledbackException {
-
+        if(this.currentConnection!=null){
+            try {
+                this.currentConnection.rollback();
+            } catch (SQLException e) {
+                throw new TransactionRolledbackException();
+            }
+        }
     }
 }
